e2c5a4ba12e8a633e9beb52ac7aef32c7de2e70d
Aditya-agarwal <adityaagarwal11122005@gmail.com>
2026-06-29 13:26:26 +0530

    Write a java program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space

--- Day1.java
+++ Day1.java
@@ -1,3 +1,34 @@
-public class Day1 {
+import java.util.Scanner;
+class Solution{
+    public void insert(int[] arr,int a,int b){
+        for(int j=arr.length-1;j>=a;j--){
+            arr[j]=arr[j-1];
+           
+        }
+        arr[a - 1] = b;
+       for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     
+    }
+}
+public class Day1 {
+    public static void main( String [] args){
+    int n;
+    System.out.println("enter n");
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    int[] arr=new int[n+1];
+    System.out.println("enter the array elements");
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+     System.out.println("enter the position");
+     int a=sc.nextInt();
+     System.out.println("enter the element");
+     int b=sc.nextInt();
+     Solution s=new Solution();
+     s.insert(arr/*arr*/,a/*pos*/,b/*to enter*/);
+     sc.close();
+}
 }
