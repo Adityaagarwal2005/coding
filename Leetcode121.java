07c494a3891fedf02fdc92c1d1b7345ce7d665d8
Aditya-agarwal <adityaagarwal11122005@gmail.com>
2026-07-10 15:37:52 +0530

    Best time to buy and sell stock

---
+++ Leetcode121.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy_price=prices[0];
+        int current_profit=0;
+        int max_profit=0;
+        for(int i=0;i<prices.length;i++){
+            current_profit=prices[i]-buy_price;
+            if(current_profit>max_profit){
+                max_profit=current_profit;
+
+            }
+
+            if(prices[i]<buy_price){
+                buy_price=prices[i];
+            }
+        }
+            return max_profit;
+    }
+    }
